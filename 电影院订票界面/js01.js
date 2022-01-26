cdf996d0252344ccdde115cc711348d3f1fc55e8
Xi_wenchuan <xq1033419145@163.com>
2022-01-26 13:29:54 +0800

    Finish。并添加了细节的md文件说明。

--- 电影院订票界面/js01.js
+++ 电影院订票界面/js01.js
@@ -9,32 +9,61 @@ const allseats = document.getElementById('AllSeats')
 
 let ticketPrice = movieSelect.value;
 
+updateUI();
+updateCount();
+
 function updateUI(){
-    const selectedSeats = document.querySelectorAll('.row .selected');
-    const selectedSeatsIndexs = selectedSeats.map(seat => selectedSeats.indexOf(seat));
+    const selectedSeatsIndexs = JSON.parse(localStorage.getItem('selectedSeatsIndexs0'));
+
+    if(selectedSeatsIndexs !==null && selectedSeatsIndexs.length > 0){
+        seats.forEach((seat, index) => {
+            if (selectedSeatsIndexs.indexOf(index) > -1) {
+            seat.classList.add('selected');
+          };
+        });
+        };
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex0')
+    movieSelect.selectedIndex = selectedMovieIndex;
+};
+
+
+
+function updateCount(){
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeatsIndexs = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats0', JSON.stringify(selectedSeats));
+    localStorage.setItem('selectedSeatsIndexs0', JSON.stringify(selectedSeatsIndexs));
+
     const selectedSeatsNumber = selectedSeats.length;
 
     number.innerText = selectedSeatsNumber;
     total.innerText = selectedSeatsNumber * ticketPrice;
 
-
+    localStorage.setItem('selectedMovieIndex0', movieSelect.selectedIndex)
+    localStorage.setItem('selectedMoviePrice0', movieSelect.value)
 
 };
 
 allseats.addEventListener('click', function(e){
     if(
-        e.target.classlist.contains('seat')&&
-        !e.target.classlist.contains('occupied')
+        e.target.classList.contains('seat') && !e.target.classList.contains('occupied')
     ){
-        e.target.classlist.toggle('selected');
+        e.target.classList.toggle('selected');
+        updateCount();
     }
     
-    updateUI();
-    }, false );
+    
+    });
 
 movieSelect.addEventListener('change', e => {
-        ticketPrice = +e.target.value;
-        updateUI();
+        ticketPrice = e.target.value;
+        localStorage.setItem('selectedMovieIndex0', e.target.selectedIndex);
+        localStorage.setItem('selectedMoviePrice0', e.target.value);
+        ;
+        updateCount();
       });
 
+updateCount();
 
