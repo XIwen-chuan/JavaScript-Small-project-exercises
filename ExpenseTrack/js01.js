5b6933a82b5ab87b8fa3d39a6d0b112a3e1133cf
Xi_wenchuan <xq1033419145@163.com>
2022-02-15 23:46:25 +0800

    Update js01.js

--- ExpenseTrack/js01.js
+++ ExpenseTrack/js01.js
@@ -1,11 +1,14 @@
 const list = document.getElementById('list');
 const deleteBtns = document.getElementsByClassName('delete-btn');
 const submitBtn = document.getElementsByClassName('btn')[0];
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
 
 
 //初始化界面
 function updateUI() {
     storageResult = localStorage.getItem('storageResult');
+    //storageResult的数据结构：[ [name1,amount1],[name2,amount2],...,[nameN,amountN] ]
     for (let index = 0; index < storageResult.length; index++) {
         let name = storageResult(index)[0];
         let amount = storageResult(index)[1];
@@ -33,13 +36,19 @@ function storageUpdate() {
 }
 
 //增加项并更新页面
-function addItemAndUpdate(event) {
+function addItemAndUpdate() {
+    let name = text.innerText;
+    let money = amount.innerText;
 
+    storageResult = localStorage.getItem('storageResult');
+    storageResult.push([name, money]);
+    localStorage.setItem('storageResult', storageResult)
+    updateUI();
 }
 
 //减少项并更新页面
 function reduceItemAndUpdate(event) {
-
+    let deleteBtn
 }
 
 //事件：项的增加
